15b6e24f374fc10e362316a09ab2642dc521abbb
Nikita Pantsevich <pantsevich15@yandex.ru>
2022-11-10 02:29:57 +0300

    did task1

--- src/Homework.java
+++ src/Homework.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Homework {
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        task1();
+    }
 
+    public static void task1() {
+        System.out.print("Enter the number: ");
+        int input = scanner.nextInt();
+        if (input % 2 == 0) {
+            System.out.println("This number is even");
+        } else {
+            System.out.println("This number is odd");
+        }
     }
 }
